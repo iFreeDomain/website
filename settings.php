c3c225fe7e68e037eb5423d4d24322a22cddbeb4
iFreeDomain <95571453+iFreeDomain@users.noreply.github.com>
2022-02-27 17:12:51 +0800

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+//Shit i made that makes the site cleaner and easier to set up - Flarf
+define("SITENAME","Project Vapor");
+define("CURRENCYNAME","Diamond/'s");
+define("SITEDOMAIN","https://projvap.cf");
+define("SMALLLOGOPATH","https://projvap.cf/img/logosmall.png");
+define("FULLLOGOPATH","https://projvap.cf/img/logo.png?v1");
+define("YOUTUBETRAILERLINK","https://www.youtube-nocookie.com/embed/Mvf9JCG2KaE");
+?>
