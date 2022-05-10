83412851da2081163e583c022b925d629be138f2
iFreeDomain <95571453+iFreeDomain@users.noreply.github.com>
2022-05-10 14:45:54 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?php
 //Shit i made that makes the site cleaner and easier to set up - Flarf
-define("SITENAME","Project Vapor");
-define("CURRENCYNAME","Diamond/'s");
-define("SITEDOMAIN","https://projvap.cf");
-define("SMALLLOGOPATH","https://projvap.cf/img/logosmall.png");
-define("FULLLOGOPATH","https://projvap.cf/img/logo.png?v1");
-define("YOUTUBETRAILERLINK","https://www.youtube-nocookie.com/embed/Mvf9JCG2KaE");
+define("SITENAME","FallenNodes");
+define("CURRENCYNAME","USD");
+define("SITEDOMAIN","http://www.ipv6c.my.id/");
+define("SMALLLOGOPATH","http://www.ipv6c.my.id/img/logosmall.png");
+define("FULLLOGOPATH","http://www.ipv6c.my.id/img/logo.png?v1");
+define("YOUTUBETRAILERLINK","");
 ?>
